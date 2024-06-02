fee2fad05b288f88a1a8e2b7463cae05020110df
Ben Hilger <bhilger78@gmail.com>
2024-06-01 23:52:45 -0400

    feature: add ability to change between months on calendar

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,25 +2,56 @@
 
 import Calendar from "@/components/calendar/calendar";
 import {useState} from "react";
-import {changeMonth} from "@/app/_lib/date";
+import {incrementMonth} from "@/app/_lib/date";
 
 export default function Home() {
 
     const [selectedDate, setSelectedDate] = useState<Date>(new Date(2024, 0, 1, 0, 0, 0, 0))
 
-    function nextMonth() {
-        const newDate = changeMonth(selectedDate, 1)
+    function goToNextMonth() {
+        const newDate = incrementMonth(selectedDate, 1)
         setSelectedDate(newDate)
     }
 
-    function prevMonth() {
-        const newDate = changeMonth(selectedDate, -1)
+    function goToPrevMonth() {
+        const newDate = incrementMonth(selectedDate, -1)
         setSelectedDate(newDate)
+    }
+
+    function currentMonth() {
+        const month = selectedDate.getMonth()
+        return Intl.DateTimeFormat(undefined, {
+            month: 'long',
+            year: 'numeric'
+        }).format(selectedDate)
     }
 
     return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Calendar onChange={setSelectedDate} selectedDate={selectedDate} month={0} year={2024}></Calendar>
+    <main className="flex min-h-screen flex-col items-center p-24">
+        <div className={"text-3xl flex items-center justify-center gap-3 font-bold mb-5"}>
+            <kbd
+                onClick={goToPrevMonth}
+                className="cursor-pointer rtl:rotate-180 inline-flex items-center px-2 py-2 text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
+                <svg className="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                     viewBox="0 0 10 16">
+                    <path
+                        d="M8.766.566A2 2 0 0 0 6.586 1L1 6.586a2 2 0 0 0 0 2.828L6.586 15A2 2 0 0 0 10 13.586V2.414A2 2 0 0 0 8.766.566Z"/>
+                </svg>
+                <span className="sr-only">Arrow key left</span>
+            </kbd>
+            {currentMonth()}
+            <kbd
+                onClick={goToNextMonth}
+                className="cursor-pointer rtl:rotate-180 inline-flex items-center px-2 py-2 text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">
+                <svg className="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                     viewBox="0 0 10 16">
+                    <path d="M3.414 1A2 2 0 0 0 0 2.414v11.172A2 2 0 0 0 3.414 15L9 9.414a2 2 0 0 0 0-2.828L3.414 1Z"/>
+                </svg>
+                <span className="sr-only">Arrow key right</span>
+            </kbd>
+
+        </div>
+        <Calendar onChange={setSelectedDate} selectedDate={selectedDate}></Calendar>
     </main>
-  );
+    );
 }
--- web/src/components/calendar/calendar.tsx
+++ web/src/components/calendar/calendar.tsx
@@ -2,8 +2,6 @@ import CalendarCell from "@/components/calendar/calendarcell";
 import {getDatesInRange, getFirstDayOfWeek, getLastDayOfWeek} from "@/app/_lib/calendar";
 
 interface CalendarProps {
-    month: number
-    year: number
     selectedDate: Date
     onChange: (date: Date) => void
 }
@@ -11,8 +9,8 @@ interface CalendarProps {
 export default function Calendar(props: CalendarProps) {
 
     function getDatesToDisplay(): Date[] {
-        const startDate = new Date(props.year, props.month, 1, 0, 0, 0, 0)
-        const endDate = new Date(props.year, props.month + 1, 0, 0, 0, 0, 0)
+        const startDate = new Date(props.selectedDate.getFullYear(), props.selectedDate.getMonth(), 1, 0, 0, 0, 0)
+        const endDate = new Date(props.selectedDate.getFullYear(), props.selectedDate.getMonth() + 1, 0, 0, 0, 0, 0)
 
         const startDay = getFirstDayOfWeek(startDate);
         const endDay = getLastDayOfWeek(endDate);
@@ -27,10 +25,10 @@ export default function Calendar(props: CalendarProps) {
     function buildCalendar() {
         const dates = getDatesToDisplay();
 
-        return dates.map((date: Date, index: number) => {
+        return dates.map((date: Date) => {
             return <CalendarCell date={date}
-                                 key={index}
-                                 disabled={date.getMonth() !== props.month}
+                                 key={date.toDateString()}
+                                 disabled={date.getMonth() !== props.selectedDate.getMonth()}
                                  selected={props.selectedDate.toDateString() === date.toDateString()}
                                  onClick={selectDate}>
                     </CalendarCell>
