63d262b8ae9e62ba3b67280a664a8a316f9db342
Ben Hilger <bhilger78@gmail.com>
2024-06-03 02:00:12 -0400

    feat: move database to env file for production

--- backend/src/main/java/com/benhilger/gratitude/database/PostgresDatabase.java
+++ backend/src/main/java/com/benhilger/gratitude/database/PostgresDatabase.java
@@ -1,27 +1,25 @@
 package com.benhilger.gratitude.database;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Map;
 
 @Configuration
 public class PostgresDatabase {
 
-    @Autowired
-    private Database environment;
-
     @Bean
     public Connection getConnection() {
+        Map<String, String> env = System.getenv();
         try {
             DataSourceBuilder<?> builder = DataSourceBuilder.create();
-            builder.driverClassName(this.environment.getDriverClassName());
-            builder.url(this.environment.getUrl());
-            builder.username(this.environment.getUsername());
-            builder.password(this.environment.getPassword());
+            builder.driverClassName(env.get("DB_DRIVER_CLASS"));
+            builder.url(env.get("DB_URL"));
+            builder.username(env.get("DB_USER"));
+            builder.password(env.get("DB_PASS"));
             return builder.build().getConnection();
         } catch (SQLException exception) {
             System.out.println("unable to connect to database: " + exception.getMessage());
