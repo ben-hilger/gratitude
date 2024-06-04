14017fd1a60527057997e2efa1c86811bc48587d
Ben Hilger <bhilger78@gmail.com>
2024-06-04 16:04:40 -0400

    feat: update schema file

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,30 @@
-CREATE DATABASE gratitude;
+create table if not exists users
+(
+    id              uuid not null
+        primary key,
+    name            varchar(200),
+    email           varchar(200),
+    hashed_password varchar(500)
+);
+
+create table if not exists gratitude_entries
+(
+    id                 uuid not null
+        primary key,
+    message            varchar(1000),
+    date_gratitude_utc date not null,
+    date_added_utc     date default now(),
+    date_modified_utc  date default now(),
+    user_id            uuid
+        references users
+);
+
+create table if not exists user_sessions
+(
+    id               uuid,
+    user_id          uuid
+        references users,
+    expires_date_utc date
+);
+
+
